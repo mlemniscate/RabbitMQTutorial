2ad0618091422f3901011bcecb4b66aa6d46231c
Milad <miladabdi1374@gmail.com>
2023-01-08 11:41:10 +0330

    add basic ack for getting an acknowledgement in message broker to know that one worker done the task

--- Worker/Worker.cs
+++ Worker/Worker.cs
@@ -25,11 +25,15 @@ consumer.Received += (model, ea) =>
     int dots = message.Split('.').Length - 1;
     Thread.Sleep( dots * 1000);
     Console.WriteLine(" [x] Done");
+
+    channel.BasicAck(
+        deliveryTag: ea.DeliveryTag,
+        multiple: false);
 };
 
 channel.BasicConsume(
     queue: "hello",
-    autoAck: true,
+    autoAck: false,
     consumer: consumer);
 
 Console.WriteLine(" Press [enter] to exit.");
