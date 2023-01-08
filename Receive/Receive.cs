bbca8de7e029c4bd57538178031ffbe1ed2e37e0
Milad <miladabdi1374@gmail.com>
2023-01-08 10:14:19 +0330

    add a simple connection with rabbit mq that producer send a request and consumer receive it and print

--- Receive/Receive.cs
+++ Receive/Receive.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare(
+    queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+};
+
+channel.BasicConsume(
+    queue: "hello",
+    autoAck: true,
+    consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
--- Send/Send.cs
+++ Send/Send.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(
+        queue: "hello",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null);
+
+    string message = "Hello World!";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(
+        exchange: "",
+        routingKey: "hello",
+        basicProperties: null,
+        body: body);
+
+    Console.WriteLine($" [x] Sent {message}");
+}
